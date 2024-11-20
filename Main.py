aad291dbb9522293947f14c57568eca040b9ffac
andreazatti <andrea1.zatti@mail.polimi.it>
2024-11-20 17:29:54 +0100

    Implementing the board and qrcode creation for the realization of the final 3D model.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from APIs.APIFunction import lichess_API, chess_com_API
+from TDModeling.TDFunctions import qr_code_creation, board_creation
 
 def main():
 
@@ -8,13 +9,13 @@ def main():
             if api == 0 or api == 1:
                 if api == 0:
                     fen, link = lichess_API()
-                    #qr_code_creation(link)
-                    #board_creation(fen)
+                    qr_code_creation(link)
+                    board_creation(fen)
                     break
                 else:
                     fen, link = chess_com_API()
-                    #qr_code_creation(link)
-                    #board_creation(fen)
+                    qr_code_creation(link)
+                    board_creation(fen)
                     break
         except ValueError:
             print("Error: please enter a valid numeric value.")
